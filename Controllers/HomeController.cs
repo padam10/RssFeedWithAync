f2574dfcf5c8e01335c4d6753aa2fb32e8714a2d
Padam Bhandari <Padam.Bhandari@datascan.com>
2016-03-15 14:57:49 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RssfeedByAyncAndAwait.Controllers
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public async Task<ActionResult> Index()
+        {
+            using (var client = new HttpClient())
+            {
+                var httpMessage = await client.GetAsync("https://channel9.msdn.com/feeds/rss").ConfigureAwait(false);
+                var content = await httpMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
+                return this.Content(content);
+            }
+        }
+    }
+}
